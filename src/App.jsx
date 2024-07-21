c4aad28479dc0a0d236249fcd36f172a8e1ab22b
astha singh <92517275+Asthasing@users.noreply.github.com>
2024-07-21 14:10:04 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import Manager from './components/Manager';
+import Footer from './components/Footer';
+
+function App() {
+  
+  return (
+    <>
+       <Navbar/>
+       <Manager/>
+       <Footer/>
+      <div >
+      </div>
+      
+    </>
+  )
+}
+
+export default App
+
